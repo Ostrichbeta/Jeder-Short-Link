89386e887fb96d0e67c46cb44cff19abde2a49d7
ostrichb <ostrichb@outlook.com>
2024-08-01 09:47:31 +0000

    Updated webui frontend

---
+++ webui-panel/assets/userinfo.36092a26.js
@@ -0,0 +1,1 @@
+import{ao as e}from"./index.3a866874.js";const a=e("userInfo",{state:()=>({isLoggedIn:!1,username:"",name:"",role:-1,avatarImgSource:""})});export{a as u};
--- webui-panel/assets/userinfo.bb1373b7.js
+++
@@ -1,1 +0,0 @@
-import{ao as e}from"./index.db56ac98.js";const a=e("userInfo",{state:()=>({isLoggedIn:!1,username:"",name:"",role:-1,avatarImgSource:""})});export{a as u};
